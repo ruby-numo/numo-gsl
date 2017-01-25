fd3189f18cc147f9e729ecc4886666f4668127f6
Masahiro TANAKA <masa16.tanaka@gmail.com>
2017-01-25 19:33:52 +0900

    modify indent

--- ext/numo/gsl/sf/gen/tmpl/sf_scalar.c
+++ ext/numo/gsl/sf/gen/tmpl/sf_scalar.c
@@ -7,23 +7,24 @@
 static VALUE
 <%=c_func%>(<%=cdef_args%>)
 {
-    <% parsed_args.each do |a| %>
+  <% parsed_args.each do |a| %>
     <%=a.def_var%><% end %>
 
-    <% args_param.each do |a| %>
+  <% args_param.each do |a| %>
     <%=a.set_value%><% end %>
 
-<%=preproc_code%>
+  <%=preproc_code%>
 
     <%=recv%><%=c_method%>(<%=c_args%>);
 
-    <% if args_out.size == 1 %>
+  <% if args_out.size == 1 %>
     return <%=args_out[0].get_value%>;
-    <% else %>
+  <% else %>
     {
         VALUE va = rb_ary_new();
         <% args_out.each do |a| %>
         <%=a.store_to_array("va")%><% end %>
         return va;
-    }<% end %>
+    }
+  <% end %>
 }
