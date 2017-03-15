b7eef88d585b96e006e1e78875a1b88e7b18e677
Masahiro TANAKA <masa16.tanaka@gmail.com>
2017-03-15 20:49:51 +0900

    make code readable

--- ext/numo/gsl/rstat/gen/erbpp_rstat.rb
+++ ext/numo/gsl/rstat/gen/erbpp_rstat.rb
@@ -1,15 +1,15 @@
 require_relative "../../gen/erbpp2"
 require "erbpp/line_number"
 
-gsl_methods = eval(open("gen/func_def.rb").read)
-rstat_methods = []
-rquantile_methods = []
-gsl_methods.each do |h|
+gsl_list = eval(open("gen/func_def.rb").read)
+rstat_list = []
+rquantile_list = []
+gsl_list.each do |h|
   case h[:func_name]
   when /^gsl_rstat_quantile_(\w+)$/
-    rquantile_methods << h
+    rquantile_list << h
   when /^gsl_rstat_(\w+)$/
-    rstat_methods << h
+    rstat_list << h
   else
     $stderr.puts "skip "+h[:func_name]
   end
@@ -17,40 +17,27 @@ end
 
 def find_template(h,tp)
   func_type = h[:func_type]
-  arg_types = h[:args].map{|a| a[0]}
-  if /_alloc$/ =~ h[:func_name]
+  arg_types = h[:args].map{|a| a[0].sub(/^const /,"")}
+  case h[:func_name]
+  when /_free$/; nil
+  when /_alloc$/
     h[:singleton] = true
     h[:func_name] = "new"
-    r =
-      case arg_types
-      when [""], ["void"]
-        "c_new_void"
-      when ["const double"], ["double"]
-        "c_new_double"
-      else
-        $stderr.puts "skip1 "+h[:func_name]
-        nil
-      end
-    return r
-  end
-  case arg_types
-  when [tp]
-    case func_type
-    when "double"
-      "c_double_f_void"
-    when "size_t"
-      "c_sizet_f_void"
-    when "int"
-      "c_void_f_void"
-    else
-      $stderr.puts "skip "+h[:func_name]
-      nil
+    case arg_types
+    when [""], ["void"];      "c_new_void"
+    when ["double"];          "c_new_double"
     end
-  when ["const double",tp]
-    "c_self_f_DFloat"
   else
-    $stderr.puts "skip "+h[:func_name]
-    nil
+    case arg_types
+    when [tp]
+      case func_type
+      when "double";          "c_double_f_void"
+      when "size_t";          "c_sizet_f_void"
+      when "int";             "c_void_f_void"
+      end
+    when ["double",tp]
+      h[:postpose] = true;    "c_self_f_DFloat"
+    end
   end
 end
 
@@ -71,10 +58,12 @@ DefLib.new(nil,'lib_rstat') do
     set struct: "gsl_rstat_workspace"
 
     undef_alloc_func
-    rstat_methods.each do |h|
+    rstat_list.each do |h|
       if t = find_template(h, "gsl_rstat_workspace *")
         m = h[:func_name].sub(/^gsl_rstat_/,"")
         def_method(m, t, **h)
+      else
+        $stderr.puts "skip "+h[:func_name]
       end
     end
     def_alias("size", "n")
@@ -90,10 +79,12 @@ DefLib.new(nil,'lib_rstat') do
     set struct: "gsl_rstat_quantile_workspace"
 
     undef_alloc_func
-    rquantile_methods.each do |h|
+    rquantile_list.each do |h|
       if t = find_template(h, "gsl_rstat_quantile_workspace *")
         m = h[:func_name].sub(/^gsl_rstat_quantile_/,"")
         def_method(m, t, **h)
+      else
+        $stderr.puts "skip "+h[:func_name]
       end
     end
   end
