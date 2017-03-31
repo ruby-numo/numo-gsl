fc2f5390a2ae12ad682831faa88ea3d44fd0fe59
Masahiro TANAKA <masa16.tanaka@gmail.com>
2017-03-31 18:31:12 +0900

    refactoring

--- ext/numo/gsl/stats/parse_stats.rb
+++ ext/numo/gsl/stats/parse_stats.rb
@@ -1,34 +1,36 @@
 require_relative "../gen/erbpp_gsl"
 
 class DefStats < DefModule
+  include ErbppGsl
 
-  def check_func(h)
-    if /These functions are now deprecated/m =~ h[:desc]
-      $stderr.puts "depricated: #{h[:func_name]}"
-      return false
+  def lookup(h)
+    dblbk = [dbl,/\[\]$/]
+    case h
+    when FM(name:"gsl_stats_spearman");         "stats_spearman"
+    when FM(name:"gsl_stats_minmax_index");     "stats_minmax_index"
+    when FM(name:"gsl_stats_minmax");           "stats_minmax"
+    when FM(name:/_(min|max)_index$/);          "stats_1a0p_index"
+    when FM(dblbk,szt,szt);                     "stats_1a0p"
+    when FM(dblbk,szt,szt,dbl);                 "stats_1a1p"
+    when FM(dblbk,szt,szt,dbl,dbl);             "stats_1a2p"
+    when FM(dblbk,szt,dblbk,szt,szt);           "stats_2a0p"
+    when FM(dblbk,szt,dblbk,szt,szt,dbl);       "stats_2a1p"
+    when FM(dblbk,szt,dblbk,szt,szt,dbl,dbl);   "stats_2a2p"
     end
-    if /This function is now deprecated/m =~ h[:desc]
-      $stderr.puts "depricated: #{h[:func_name]}"
-      return false
-    end
-    a = [
-     StatsBasic
-    ]
-    a.each do |c|
-      if t = c.lookup(h)
-        c.new(self,t,**h)
-        return true
-      end
-    end
-    $stderr.puts "skip #{h[:func_type]} #{h[:func_name]} #{h[:args].inspect}"
-    false
   end
 
+  def check_func(h)
+    if t = lookup(h)
+      StatsBasic.new(self,t,**h)
+    else
+      $stderr.puts "skip #{h[:func_name]}"
+    end
+  end
 end
 
 
 class StatsBasic < DefMethod
-  include ErbppGsl
+  include FuncParser
 
   PARAM_DESC =
     {
@@ -41,11 +43,12 @@ class StatsBasic < DefMethod
     }
 
   def initialize(parent,tmpl,**h)
-    @preproc_code = ""
     m = h[:func_name].sub(/^gsl_stats_/,"")
-    super(parent,tmpl,name:m,**h)
+    super(parent, tmpl, name:m, singleton:true, **h)
+    parse_args(h)
     @varg = -1
     set n_arg: -1
+    @preproc_code = ""
   end
 
   def argument_property(type,name)
@@ -64,70 +67,4 @@ class StatsBasic < DefMethod
       end
     end
   end
-
-  class << self
-
-    ARGTMPL = [
-     "stats_1a0p double[] size_t size_t",
-     "stats_1a1p double[] size_t size_t double",
-     "stats_1a2p double[] size_t size_t double double",
-     "stats_2a0p double[] size_t double[] size_t size_t",
-     "stats_2a1p double[] size_t double[] size_t size_t double",
-     "stats_2a2p double[] size_t double[] size_t size_t double double",
-    ].map{|x| b=x.split; a=b.shift; [a,b]}
-
-    def find_template_from_args(args)
-      narg = args.size
-      ARGTMPL.each do |tmpl,argtype|
-        arg_ok = false
-        if narg == argtype.size
-          narg.times do |i|
-            if /^(.*)\[\]$/ =~ argtype[i]
-              type = $1
-              if /^(.*)\[\]$/ !~ args[i][1]
-                arg_ok = false
-                break
-              end
-            else
-              type = argtype[i]
-              if /^(.*)\[\]$/ =~ args[i][1]
-                arg_ok = false
-                break
-              end
-            end
-            if /(const )?#{type}/ =~ args[i][0]
-              arg_ok = true
-            else
-              arg_ok = false
-              break
-            end
-          end
-        end
-        return tmpl if arg_ok
-      end
-      false
-    end
-
-    def find_template(h)
-      case h[:func_name]
-      when "gsl_stats_spearman"
-        "stats_spearman"
-      when "gsl_stats_minmax_index"
-        "stats_minmax_index"
-      when "gsl_stats_minmax"
-        "stats_minmax"
-      when /_(min|max)_index$/
-        "stats_1a0p_index"
-      else
-        find_template_from_args(h[:args])
-      end
-    end
-
-    def lookup(h)
-      tmpl = find_template(h)
-      return tmpl if File.exist?("tmpl/#{tmpl}.c")
-    end
-
-  end
-
 end
