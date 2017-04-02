5c71f9534f52a175d31f341fb6be9c6c4a69369a
Masahiro TANAKA <masa16.tanaka@gmail.com>
2017-04-02 17:48:23 +0900

    include numo_gsl.h in lib.c

--- ext/numo/gsl/gen/tmpl/lib.c
+++ ext/numo/gsl/gen/tmpl/lib.c
@@ -7,34 +7,13 @@
 */
 
 #include <ruby.h>
+#include <assert.h>
 #include "numo/narray.h"
 #include "numo/template.h"
+#include "../numo_gsl.h"
 <% include_files.each do |f| %>
 #include <<%=f%>>
 <% end %>
-
-#define GET_PTR(lp,i) (((lp)->args[i]).ptr + ((lp)->args[i].iter[0]).pos)
-
-#if SIZEOF_INT == 4
-#define cI numo_cInt32
-#define cUI numo_cUInt32
-#elif SIZEOF_INT==8
-#define cI numo_cInt64
-#define cUI numo_cUInt64
-#endif
-
-#if SIZEOF_SIZE_T == 4
-#define cSZ numo_cUInt32
-#define cSSZ numo_cInt32
-#elif SIZEOF_SIZE_T == 8
-#define cSZ numo_cUInt64
-#define cSSZ numo_cInt64
-#endif
-
-#define cDF numo_cDFloat
-#define cDC numo_cDComplex
-#define cInt cI
-#define cUInt cUI
 
 static VALUE <%=ns_var%>;
 
